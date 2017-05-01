a3010233ca61a5bfb29d3861956e4af0f8c764a9
Someel Pagaria <spagaria@blanca.uccs.edu>
2017-04-30 18:41:44 -0600

    added stop and wait to server

--- project2/udp_server.c
+++ project2/udp_server.c
@@ -12,24 +12,31 @@
 #define SERVER_UDP_PORT 2559 /* arbitrary, but client and server
 must agree */
 #define MAXLEN 4096 /* block transfer size */
+
+int shoulddrop(float prob);
+
 int main(int argc, char *argv[])
 {
-	int  sd, client_len, port, n, bytes, fd;  
-	char  buf[MAXLEN], sendbuf[MAXLEN];  
+	int  sd, client_len, port, n, m, bytes, fd, arqprot, ackbuf;  
+	char  buf[MAXLEN], sendbuf[MAXLEN], strackbuf[4];
 	struct  sockaddr_in  server, client;
-	struct timeval start, end; 
+	struct timeval start, end;
+	double elapsedTime;
 	float prob = 0.1;
-	printf("beginnning of fn\n");
+	arqprot = 3;
+	printf("beginning of fn\n");
  
  	switch(argc) {  
 	case 1:   
-		port = SERVER_UDP_PORT;   
+		port = SERVER_UDP_PORT;
+		arqprot = atoi(argv[1]);
 		break;  
 	case 2:   
-		port = atoi(argv[1]);   
+		port = atoi(argv[1]);
+		arqprot = atoi(argv[2]);
 		break;  
 	default:   
-		fprintf(stderr, "Usage: %s [port]\n", argv[0]);   exit(1);  
+		fprintf(stderr, "Usage: %s [port] [protocol]\n", argv[0]);   exit(1);  
 	}
 
 	/* Create a datagram socket */
@@ -50,7 +57,168 @@ int main(int argc, char *argv[])
 	}
 	printf("before while loop\n");	
 	int sendfile = 0;
-	while (1) {   
+	
+	while (arqprot == 1) {   
+		
+		printf("beginning of while loop\n");
+		client_len = sizeof(client);   
+		printf("got past client_len = ..\n");
+		sendfile = 0;
+		
+		if ((n = recvfrom(sd, buf, MAXLEN, 0, (struct sockaddr *)&client, &client_len)) < 0) {         
+		fprintf(stderr, "Can't receive datagram\n");         
+		exit(1);
+		}
+		else
+		{
+			printf("buf: %s\n", buf);
+			printf("n: %i\n", n);    
+		}
+		
+		if (n>0)
+			sendfile = 1;
+			fd = open(buf, O_RDONLY);
+			if (fd<0) 
+			{
+				fprintf(stderr,"open failed");
+				exit(1);
+			}
+		printf("made it this far...\n");	
+  		//for testing, right now we're just sending the filename
+  		//back to the client
+  		/*
+		if (sendto(sd, buf, strlen(buf), 0,    
+		(struct sockaddr *)&client, client_len) != n) {         
+			fprintf(stderr, "Can't send datagram\n");         
+			exit(1);   
+		}
+		*/
+
+		//loop to start sending blocks/packets
+		while(sendfile == 1) {
+			bytes = read(fd, sendbuf, MAXLEN);
+			printf("bytes: %i\n", bytes);
+			if (bytes == 0) break; //end of file
+			if (bytes == -1)
+			{
+				printf("bytes returned: %i\n", bytes);
+				printf("exiting with error..\n");
+				exit(1);
+			}
+			/*
+			if (sendto(sd, sendbuf, bytes, 0,
+			(struct sockaddr *)&client, client_len) != strlen(sendbuf)) {
+				fprintf(stderr, "can't send datagram\n");
+				exit(1);
+			}
+			*/
+			
+			gettimeofday(&start, NULL);
+			
+			int drop = shoulddrop(prob);
+			if(drop != 1)
+			{
+				m = sendto(sd, sendbuf, bytes, 0, (struct sockaddr *)&client, client_len);
+			}
+			else
+			{
+				printf("Packet was dropped\n");
+			}
+			//printf("sendbuf sent: %s\n", sendbuf);
+			while(1)
+			{
+				if((n = recvfrom(sd, strackbuf, MAXLEN, 0, (struct sockaddr *)&client, &client_len)) < 0) {         
+					gettimeofday(&end, NULL);
+					elapsedTime = (end.tv_sec - start.tv_sec) * 1000.0;
+					if(elapsedTime >= 300)
+					{
+						drop = shoulddrop(prob);
+						if(drop != 1)
+						{
+							m = sendto(sd, sendbuf, bytes, 0, (struct sockaddr *)&client, client_len);
+						}
+						else
+						{
+							printf("Packet was dropped\n");
+						}
+						gettimeofday(&start, NULL);
+						continue;
+					}
+					continue;
+				}	
+			}
+			ackbuf = atoi(strackbuf);
+			if(ackbuf == m)
+			{
+				printf("acknowledgement Recieved..\n");
+			}
+		}
+		printf("finished sending file..\n");
+		close(fd);   
+	}
+	
+	while (arqprot == 2) {   
+		printf("beginning of while loop\n");
+		client_len = sizeof(client);   
+		printf("got past client_len = ..\n");
+		sendfile = 0;
+		if ((n = recvfrom(sd, buf, MAXLEN, 0, (struct sockaddr *)&client, &client_len)) < 0) {         
+		fprintf(stderr, "Can't receive datagram\n");         
+		exit(1);
+		}
+		else
+		{
+			printf("buf: %s\n", buf);
+			printf("n: %i\n", n);    
+		}
+		if (n>0)
+			sendfile = 1;
+			fd = open(buf, O_RDONLY);
+			if (fd<0) 
+			{
+				fprintf(stderr,"open failed");
+				exit(1);
+			}
+		printf("made it this far...\n");	
+  		//for testing, right now we're just sending the filename
+  		//back to the client
+  		/*
+		if (sendto(sd, buf, strlen(buf), 0,    
+		(struct sockaddr *)&client, client_len) != n) {         
+			fprintf(stderr, "Can't send datagram\n");         
+			exit(1);   
+		}
+		*/
+
+		//loop to start sending blocks/packets
+		while(sendfile == 1) {
+			bytes = read(fd, sendbuf, MAXLEN);
+			printf("bytes: %i\n", bytes);
+			if (bytes == 0) break; //end of file
+			if (bytes == -1)
+			{
+				printf("bytes returned: %i\n", bytes);
+				printf("exiting with error..\n");
+				exit(1);
+			}
+			/*
+			if (sendto(sd, sendbuf, bytes, 0,
+			(struct sockaddr *)&client, client_len) != strlen(sendbuf)) {
+				fprintf(stderr, "can't send datagram\n");
+				exit(1);
+			}
+			*/
+			
+			sendto(sd, sendbuf, bytes, 0,
+                        (struct sockaddr *)&client, client_len);
+			//printf("sendbuf sent: %s\n", sendbuf);
+			usleep(300);
+		}
+		printf("finished sending file..\n");
+		close(fd);   
+	}
+	
+	while (arqprot == 3) {   
 		printf("beginning of while loop\n");
 		client_len = sizeof(client);   
 		printf("got past client_len = ..\n");
@@ -113,14 +281,15 @@ int main(int argc, char *argv[])
 	close(sd);
 	return(0);
 }
-int drop_packet(float prob)
+
+int shoulddrop(float prob)
 {
 	srand(time(NULL));
 	int random_number = rand();
 	int n = 9;
 	int rand_capped = random_number % n;//between 0 and 9
 	
-	int amount = prob * 10;
+	int amount = (int)(prob * 10);
 	int actualhigh = amount - 1;
 	int actuallow = 0;
 	int dodrop = 0;
